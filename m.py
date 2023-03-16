393330c844b3ba437b3e5cd30befd265e35831bc
supersigmatdurai <tdurai@supersigmasolutions.com>
2023-03-16 19:39:49 +0530

    completed

--- m.py
+++
@@ -1,9 +0,0 @@
-from geopy.geocoders import Nominatim
-
-# Initialize Nominatim API
-geolocator = Nominatim(user_agent="MyApp")
-
-location = geolocator.geocode("Port Hedland,Western Australia,Australia")
-
-print("The latitude of the location is: ", location.latitude)
-print("The longitude of the location is: ", location.longitude)
